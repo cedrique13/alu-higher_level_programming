a0f680b1d7f7cf70dc495a22511bd2414d30b5f7
cedrique13 <c.bienvenue@alustudent.com>
2025-04-02 12:03:19 +0000

    Added User-Agent header on task 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
-
+"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
 
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
+headers = {"User-Agent": "Mozilla/5.0"}
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+request = urllib.request.Request(url, headers=headers)
+
+with urllib.request.urlopen(request) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
+
