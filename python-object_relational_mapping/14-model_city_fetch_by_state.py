1be4206c62426e7e792e8131671b7352f1a54fc4
cedrique13 <c.bienvenue@alustudent.com>
2025-04-05 03:35:35 -0700

    executable 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""
-Script that prints all City objects from the database
-"""
-from model_city import City
-from model_state import Base, State
+"""Prints all City objects from the database, grouped by State."""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import Base, State
+from model_city import City
+import sys
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
-    Base.metadata.create_all(engine)
+    # Get credentials
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    city = session.query(State, City).join(City).order_by(City.id)
-    for state, city in city:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    # Close session
-    session.close()
+    # Setup engine and session
+    db_url = f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all cities with their corresponding state
+    results = (
+        session.query(State.name, City.id, City.name)
+        .join(City, City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
+
+    # Print in the specified format
+    for state_name, city_id, city_name in results:
+        print(f"{state_name}: ({city_id}) {city_name}")
+
